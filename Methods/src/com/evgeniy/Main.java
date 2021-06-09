fb4452c295330b8ac43e586ab9de0db95c120811
evgeniy <eanekrasovart@gmail.com>
2021-06-09 12:10:57 +0200

    Resolving code with one return

--- Methods/src/com/evgeniy/Main.java
+++ Methods/src/com/evgeniy/Main.java
@@ -44,14 +44,25 @@ public class Main {
     }
 
     public static int calculateHighScorePosition(int playerScore) {
+//        if (playerScore >= 1000) {
+//            return 1;
+//        } else if (playerScore >= 500 && playerScore < 1000) {
+//            return 2;
+//        } else if (playerScore >= 100 && playerScore < 500) {
+//            return 3;
+//        } else {
+//            return 4;
+//        }
+
+        // Resolving problem with one return
+        int position = 4;
         if (playerScore >= 1000) {
-            return 1;
-        } else if (playerScore >= 500 && playerScore < 1000) {
-            return 2;
-        } else if (playerScore >= 100 && playerScore < 500) {
-            return 3;
-        } else {
-            return 4;
+            position = 1;
+        } else if (playerScore >= 500) {
+            position = 2;
+        } else if (playerScore >= 100) {
+            position = 3;
         }
+        return position;
     }
 }
