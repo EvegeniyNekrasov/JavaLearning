7c80cc2f093e851c9713bfc1f0b56ec122e80208
evgeniy <eanekrasovart@gmail.com>
2021-06-11 12:28:36 +0200

    Minutes To Years and Days challenge

---
+++ MinutesToYearsAndDaysCalculator/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        MinutesToYearsDaysCalculator.printYearsAndDays(525600); // output 525600 min = 1 y and 0 d
+        MinutesToYearsDaysCalculator.printYearsAndDays(1051200); // output 1051200 min = 2 y and 0 d
+        MinutesToYearsDaysCalculator.printYearsAndDays(561600); // output 561600 min = 1 y and 25 d
+    }
+}
