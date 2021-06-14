9ca0339b72d9bc99734390f9733009a099949588
evgeniy <eanekrasovart@gmail.com>
2021-06-14 14:00:54 +0200

    Shared Digit challenge

---
+++ SharedDigit/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean test = SharedDigit.hasSharedDigit(15, 55);
+        System.out.println(test); // output true
+    }
+}
