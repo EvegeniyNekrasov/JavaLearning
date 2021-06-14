d48a4f97c95678e29b88867cff0293ab7843d1c9
evgeniy <eanekrasovart@gmail.com>
2021-06-14 13:34:20 +0200

    Even Digit Sum challenge

---
+++ EvenDigitSum/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        int test = EvenDigitSum.getEvenDigitSum(123456789);
+        System.out.println(test); // output 20
+    }
+}
